52e4d71018fdcf7f998099f04771a9d36ef0cd77
DaneSteenkamp <danesteenkamp.designer@gmail.com>
2021-04-08 10:15:12 +0200

    Changes made to Projects

--- components/Projects_info.js
+++ components/Projects_info.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Project_info = () => (
   // This function provides the link to my Project page
@@ -13,19 +14,26 @@ const Project_info = () => (
     </style>
     <ul>
       <li>
-        <Link href="https://github.com/DaneSteenkamp/Capstone_ItunesB">
-          <a>
-            <u>Project 1 Itunes Search App</u>
-          </a>
-        </Link>
-      </li>
-      <br></br>
-      <li>
         <Link href="https://game-danesteenkampdesigner-gmailcom.vercel.app/">
           <a>
-            <u>Project 2 Memory Game</u>
+            <u>Project 1 Memory Game</u>
           </a>
         </Link>
+        <h5>Libraries & Frameworkes Used </h5>
+        <Image src="/react-logo.png" width={150} height={50} />
+      </li>
+
+      <br></br>
+      <li>
+        <Link href="https://github.com/DaneSteenkamp/Itunes-App.git">
+          <a>
+            <u>Project 2 Itunes Search App</u>
+          </a>
+        </Link>
+        <h5>Libraries & Frameworkes Used </h5>
+        <Image src="/react-logo.png" width={150} height={50} />
+        <Image src="/express.png" width={150} height={50} />
+        <Image src="/node.png" width={75} height={75} />
       </li>
     </ul>
   </div>
